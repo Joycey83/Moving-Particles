7170c016839e4000dad919d236ceb600dc26023c
Joyce <Jcheung8385@gmail.com>
2024-03-11 12:50:29 +0000

    create a Particle constructor class, effect constructor class and a animate function

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ const ctx = canvas.getContext("2d"); // canvas rendering context 2d
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 console.log(ctx);
+
+class Particle {
+  constructor() {}
+}
+
+class Effect {
+  constructor() {}
+}
+
+function animate() {}
