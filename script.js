87ea09dc732c30bde6032d4bff0fec61a72d45b5
Joyce <Jcheung8385@gmail.com>
2024-03-11 12:46:20 +0000

    console.log ctx, to see what types of properties that I want access to for context 2d rendering

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d"); // canvas rendering context 2d
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+console.log(ctx);
