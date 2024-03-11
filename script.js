7ab1ce6703a8a9a4ab0b01c3a455e5580a1a1d3f
Joyce <Jcheung8385@gmail.com>
2024-03-11 13:22:56 +0000

    add a draw method to define each particle on what they look like, size and color etc

--- script.js
+++ script.js
@@ -15,6 +15,13 @@ class Particle {
     this.vx = Math.random() * 1 - 0.5;
     this.vy = Math.random() * 1 - 0.5;
   }
+  draw(context) {
+    context.fillStyle = "hsl(" + this.x * 0.5 + ", 100%, 50%)";
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    context.fill();
+    context.stroke();
+  }
 }
 
 class Effect {
